38e95e0b522ea8166e93283727dd6dbf215c58bc
Tang <forbiddensite403@hotmail.com>
2019-11-02 22:57:04 +0800

    Use ReactJS preset

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
-require('./bootstrap');
+// app.js
+
+require("./bootstrap");
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Master from "./components/Master";
+import CreateItem from "./components/CreateItem";
+
+render(
+    <BrowserRouter>
+        <Route path="/" component={Master} />
+        <Route path="/add-item" component={CreateItem} />
+    </BrowserRouter>,
+    document.getElementById("example")
+);
