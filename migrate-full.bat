b4fd50b03ebe5696cdbe5577f095a483ecf77645
Tang <tangcb1994@hotmail.com>
2020-04-03 12:43:51 +0800

    Update migrate batch file to include storage init command

--- migrate-full.bat
+++ migrate-full.bat
@@ -1,6 +1,8 @@
 call composer install
+call php artisan storage:link
 call php artisan migrate
 call php artisan passport:keys
+call php artisan key:generate
 call php artisan passport:client --password
 call npm install
 call npm run dev
